97b975c0c35580c508c13593b05d9d6f899ed5d3
A-Ze <mike504110403@gmail.com>
2023-07-03 23:05:56 +0800

    設定檔註釋

---
+++ MyDotNetCoreCodeBase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDotNetCoreCodeBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
